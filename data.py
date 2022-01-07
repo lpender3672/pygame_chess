2705fbacc40cf7672b1dc5669564c47156a211df
lpender3672 <58957722+lpender3672@users.noreply.github.com>
2022-01-07 12:23:33 +0000

    Add files via upload

--- data.py
+++ data.py
@@ -1,5 +1,4 @@
-
-
+import socket, pickle, select
 
 class v2(object):
     def __init__(self, x, y):
@@ -7,16 +6,12 @@ class v2(object):
         self.y = y
 
     def zero(self):
-        return v2(0,0)
+        self.x = 0
+        self.y = 0
 
     def inlist(self, list):
         for i in list:
             if i.x == self.x and i.y == self.y:
-                return True
-        return False
-    def inmoveslist(self, list):
-        for i in list:
-            if i.to.x == self.x and i.to.y == self.y:
                 return True
         return False
 
@@ -25,20 +20,41 @@ class v2(object):
             return True
         return False
 
-alongx = {"h":0,  "g":1,  "f":2,  "e":3,  "d":4,  "c":5,  "b":6,  "a":7}
-inverse = {0: "h", 1: "g", 2: "f", 3: "e", 4: "d", 5: "c", 6: "b", 7: "a"}
 
-def getposfromhash(hash):
+class server(object):
+    def __init__(self, serversocket, buffersize):
+        self.serversocket = serversocket
+        self.buffersize = buffersize
 
-    letter = hash[0]
-    number = int(hash[1])
+    def send(self, HEADERSIZE, msg):
+        msg = bytes(f'{len(msg):<{HEADERSIZE}}', 'utf-8') + msg
+        self.serversocket.send(msg)
 
-    return v2(alongx[letter], number - 1)
+    def recieve(self, HEADERSIZE):
+        full_msg = b''
+        new_msg = True
+        msg_recived = False
 
+        self.serversocket.setblocking(1)
+        ready = select.select([self.serversocket], [], [], 0.1)
 
-def gethashfrompos(pos):
+        if ready[0]:
+            while not msg_recived:
 
-    return inverse[pos.x] +str(pos.y + 1)
+                msg = self.serversocket.recv(self.buffersize)
+                if new_msg:
+                    msglen = int(msg[:HEADERSIZE])
+                    new_msg = False
+
+                full_msg += msg
+
+                if len(full_msg) - HEADERSIZE == msglen:
+                    d = pickle.loads(full_msg[HEADERSIZE:])
+                    msg_recived = True
+
+                    return d
+        else:
+            return ""
 
 
 class piece(object):
@@ -48,99 +64,25 @@ class piece(object):
         self.unicode = piecedict[id]["unicode"]
         self.id = id
 
-class move(object):
-
-    def __init__(self, board, to, fro, hash = "", createpiece = None):
-
-        self.additionalmoves = []
-        self.to = to
-        self.fro = fro
-        self.createpiece = createpiece
-        if hash != "":
-            self.hash = hash
-            frohash = hash[0:2]
-            tohash = hash[2:4]
-
-            self.fro = getposfromhash(frohash)
-            self.to = getposfromhash(tohash)
-        else:
-            frohash = gethashfrompos(fro)
-            tohash = gethashfrompos(to)
-
-            self.hash = frohash + tohash
-
-        self.topiece = board.getpiece(self.to)
-        self.fropiece = board.getpiece(self.fro)
-
-        # additional moves needed?
-        if board.pawnenpassan and self.topiece.name == "" and self.fropiece.name == "pawn" and self.to.x != self.fro.x:
-            colourdirection = {"white":-1, "black":1}
-            d = colourdirection[self.fropiece.colour]
-            removepos = v2(self.to.x, self.to.y + d)
-            self.additionalmoves.append(move(board,removepos,self.to))
-
-        leftrookdic = {"white": v2(0, 0), "black": v2(0, 7)}
-        rightrookdic = {"white": v2(7, 0), "black": v2(7, 7)}
-        if self.fropiece.name == "king" and self.topiece.name == "" and (self.to.x - self.fro.x) == 2:
-
-            rookfropos = rightrookdic[self.fropiece.colour]
-            rooktopos = v2(self.to.x - 1, self.to.y)
-            self.additionalmoves.append(move(board,rooktopos, rookfropos))
-        elif self.fropiece.name == "king" and self.topiece.name == "" and (self.to.x - self.fro.x) == -2:
-
-            rookfropos = leftrookdic[self.fropiece.colour]
-            rooktopos = v2(self.to.x + 1, self.to.y)
-            self.additionalmoves.append(move(board, rooktopos, rookfropos))
-        # do pawn promotion
-
-        #print(additionalmoves)
-        #for mov in additionalmoves:
-        #    print(mov.fropiece.name)
-
-
-    def perform(self, board):
-        for move in self.additionalmoves:
-            move.additionalmoves = []
-            move.perform(board)
-
-        board.overwritepos(self.fro, piece(board.piecedict, "0"))
-        if self.createpiece == None:
-            board.overwritepos(self.to, self.fropiece)
-        else:
-            board.overwritepos(self.to, self.createpiece)
-
-    def unperform(self, board):
-
-        board.overwritepos(self.to, self.topiece)
-        board.overwritepos(self.fro, self.fropiece)
-
-        for move in self.additionalmoves:
-            move.additionalmoves = []
-            move.unperform(board)
-
 
 class board(object):
-    def __init__(self, layout, piecedict):
+    def __init__(self, layout, piecedict, imported = False):
         self.startlayout = layout
-        self.history = []
+        self.history = {}
         self.piecedict = piecedict
         self.check = False
-        self.pawnenpassan = False
+        if imported:
+            self.board = layout
+        else:
+            board = []
 
-        self.board = self.piecemap(layout)
-        self.startboard = self.piecemap(layout)
+            for row in layout:
+                board_row = []
+                for id in row:
+                    board_row.append(piece(self.piecedict, id))
+                board.append(board_row)
+            self.board = board
 
-
-    def piecemap(self, layout):
-        bord = []
-
-        for row in layout:
-            bord_row = []
-            for id in row:
-                bord_row.append(piece(self.piecedict, id))
-            bord.append(bord_row)
-
-        return bord
 
     def idmap(self):
         idmap = []
@@ -159,7 +101,8 @@ class board(object):
     def overwritepos(self, pos, piece):
         self.board[pos.y][pos.x] = piece
 
-
+    def saveboard(self, gamestage):
+        self.history[gamestage] = self.idmap()
 
 
     def getkingpos(self, colour):
@@ -181,12 +124,10 @@ class board(object):
                         piecespos.append(v2(x, y))
         return piecespos
 
+
     def unmovedpiece(self, pos):
-        for mov in self.history:
-            if pos.equal(mov.fro) or pos.equal(mov.to):
+        for key in self.history:
+            if self.history[key][pos.y][pos.x] != self.startlayout[pos.y][pos.x]:
                 return False
         return True
-
-    def gethistoryhash(self):
-        return [mov.hash for mov in self.history]
 
